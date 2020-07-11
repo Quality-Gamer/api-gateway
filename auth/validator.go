9e90ca92efc5d513d6ea41a9cd203bb6e1dd5fdf
Agualuza <iagoagualuza@id.uff.br>
2020-07-11 17:00:25 -0300

    Update validator.go

--- auth/validator.go
+++ auth/validator.go
@@ -1,11 +1,14 @@
 package auth
 
 import (
+	"fmt"
 	"github.com/Quality-Gamer/api-gateway/conf"
 	"github.com/Quality-Gamer/api-gateway/database"
 )
 
 func KeyValidator(key string) bool {
 	token := database.GetKey(conf.GetAuthKey())
+	fmt.Println(key)
+	fmt.Println(token)
 	return key == token
 }
